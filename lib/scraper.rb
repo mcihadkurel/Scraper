6d8c9d6bd96ec5a4606bfb0e40fc6b748e6b1fa7
Muhammed Cihad Kurel <mcihadkurel@gmail.com>
2020-09-14 02:16:14 +0300

    loop add each list

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,13 +9,21 @@ def scraper
     job_listings = parsed_page.css('li.job.inline.featured')
     job_listings.each do |job_listing|
         job = {
-            title: job_listing.css('a.jobTitle').text
-            company: job_listing.css('a.companyName').text
-            location: job_listing.css('span.jobLocation').text
-            application: job_listing.css('span.createdAt').text
-        }
-    end
-    byebug
-end
-
+            title: job_listing.css('a.jobTitle').text }
+        end
+        job_listings.each do |job_listing|
+            job = {
+                location: job_listing.css('span.jobLocation').text }
+            end
+            job_listings.each do |job_listing|
+                job = {
+                    company: job_listing.css('a.companyName').text}
+                end
+                job_listings.each do |job_listing|
+                    job = {
+                        application: job_listing.css('span.createdAt').text}
+                    end
+                end
+                byebug
+                    
 scraper

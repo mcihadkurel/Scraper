c35bb7053e545a197ff345f0a7981de71646f6f3
Muhammed Cihad Kurel <mcihadkurel@gmail.com>
2020-09-14 01:05:21 +0300

    Change URL

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,13 +3,13 @@ require 'httparty'
 require 'byebug'
 
 def scraper
-    url = "https://www.hostelworld.com/s?q=Izmir,%20Turkey&country=Turkey&city=Izmir&type=city&id=719&from=2020-09-14&to=2020-09-17&guests=2&page=1"
+    url = "https://cryptojobslist.com/"
     unparsed_page = HTTParty.get(url)
     parsed_page = Nokogiri::HTML(unparsed_page)
     hostel_lists = parsed_page.css('div.property-card')
     hostel_lists.each do |hostel_list|
         hostel = {
-            
+
         }
     end
     byebug

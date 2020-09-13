5b6e7b35a443dfc697b95179248171b50879f2e3
Muhammed Cihad Kurel <mcihadkurel@gmail.com>
2020-09-14 01:54:00 +0300

    Each loop add

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,10 +6,13 @@ def scraper
     url = "https://cryptojobslist.com/"
     unparsed_page = HTTParty.get(url)
     parsed_page = Nokogiri::HTML(unparsed_page)
-    hostel_lists = parsed_page.css('div.property-card')
-    hostel_lists.each do |hostel_list|
-        hostel = {
-
+    job_listings = parsed_page.css('li.job.inline.featured')
+    job_listings.each do |job_listing|
+        job = {
+            title: job_listing.css('a.jobTitle').text
+            company: job_listing.css('a.companyName').text
+            location: job_listing.css('span.jobLocation').text
+            application: job_listing.css('span.createdAt').text
         }
     end
     byebug

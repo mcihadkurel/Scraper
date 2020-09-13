ff4a6af08564335d455f92d0fb14396439966275
Muhammed Cihad Kurel <mcihadkurel@gmail.com>
2020-09-14 02:24:13 +0300

    Fix rubocop errors

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,27 +3,31 @@ require 'httparty'
 require 'byebug'
 
 def scraper
-    url = "https://cryptojobslist.com/"
-    unparsed_page = HTTParty.get(url)
-    parsed_page = Nokogiri::HTML(unparsed_page)
-    job_listings = parsed_page.css('li.job.inline.featured')
-    job_listings.each do |job_listing|
-        job = {
-            title: job_listing.css('a.jobTitle').text }
-        end
-        job_listings.each do |job_listing|
-            job = {
-                location: job_listing.css('span.jobLocation').text }
-            end
-            job_listings.each do |job_listing|
-                job = {
-                    company: job_listing.css('a.companyName').text}
+  url = 'https://cryptojobslist.com/'
+  unparsed_page = HTTParty.get(url)
+  parsed_page = Nokogiri::HTML(unparsed_page)
+  job_listings = parsed_page.css('li.job.inline.featured')
+  job_listings.each do |job_listing|
+    job = {
+      title: job_listing.css('a.jobTitle').text
+    }
+  end
+  job_listings.each do |job_listing|
+    job = {
+      location: job_listing.css('span.jobLocation').text
+    }
+  end
+  job_listings.each do |job_listing|
+    job = {
+      company: job_listing.css('a.companyName').text
+    }
+  end
+  job_listings.each do |job_listing|
+    job = {
+      application: job_listing.css('span.createdAt').text
+    }
+  end
                 end
-                job_listings.each do |job_listing|
-                    job = {
-                        application: job_listing.css('span.createdAt').text}
-                    end
-                end
-                byebug
-                    
+byebug
+
 scraper

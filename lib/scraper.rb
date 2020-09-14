f4bfa4eb2967353c44fd739ef28917eee65036e2
Muhammed Cihad Kurel <mcihadkurel@gmail.com>
2020-09-14 18:14:21 +0300

    Create per_page

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,16 +6,18 @@ def scraper
   url = 'https://cryptojobslist.com/'
   unparsed_page = HTTParty.get(url)
   parsed_page = Nokogiri::HTML(unparsed_page)
+  jobs = Array.new
   job_listings = parsed_page.css('li.job.inline.featured')
+
+  per_page = job_listings.count
+  
   job_listings.each do |job_listing|
     job = {
-      location: job_listing.css('span.jobLocation').text
       title: job_listing.css('a.jobTitle').text
-      company: job_listing.css('a.companyName').text
-      application: job_listing.css('span.createdAt').text
     }
+    jobs << job
   end
-                end
-byebug
+  byebug
+end
 
 scraper

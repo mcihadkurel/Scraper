bbfa418dca35598aab93da2f7e04c12c67ca62dc
Muhammed Cihad Kurel <mcihadkurel@gmail.com>
2020-09-16 15:35:01 +0300

    Fix rubocop errors

--- bin/main.rb
+++ bin/main.rb
@@ -3,15 +3,14 @@ require 'open-uri'
 require 'byebug'
 
 class Scraper
-  def initialize(url, unparsed_page, parsed_page)
+  def initialize(url, _unparsed_page, _parsed_page)
     @url = url
     @unparsed_page = ::OpenURI.open_uri(url)
     @parsed_page = Nokogiri::HTML(@unparsed_page)
   end
-  
-  
+
   def scrape
-    jobs = Array.new
+    jobs = []
     job_lists = @parsed_page.css('li.job.inline.featured')
     job_lists.each do |job_list|
       job = {

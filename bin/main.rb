4e66487a7e5af5dd7442172c0287ad8c0a320029
Muhammed Cihad Kurel <mcihadkurel@gmail.com>
2020-09-16 15:22:33 +0300

    Loop style complete

--- bin/main.rb
+++ bin/main.rb
@@ -1,5 +1,6 @@
 require 'nokogiri'
 require 'open-uri'
+require 'byebug'
 
 class Scraper
   def initialize(url, _unparsed_page, _parsed_page)
@@ -7,11 +8,11 @@ class Scraper
     @unparsed_page = ::OpenURI.open_uri(url)
     @parsed_page = Nokogiri::HTML(@unparsed_page)
   end
-
-  def web_scraper
+  
+  
+  def scrape
     jobs = Array.new
     job_lists = @parsed_page.css('li.job.inline.featured')
-    i = 1
     job_lists.each do |job_list|
       job = {
         title: job_list.css('a.jobTitle').text,
@@ -19,13 +20,16 @@ class Scraper
         location: job_list.css('span.jobLocation').text,
         application: job_list.css('span.applications').text
       }
-      job.each { |key, value| puts "#{key.capitalize} : #{value.capitalize}" }
-      i += 1
+      job.each do |key, value|
+        puts "||#{key.capitalize} : #{value.capitalize}||"
+        puts '--------------'
+      end
+      puts '            '
+      puts '            '
       jobs << job
     end
     jobs.count
   end
 end
 
-scrapping = Scraper.new('https://cryptojobslist.com/', @unparsed_page, @parsed_page)
-scrapping.web_scraper
+Scraper.new('https://cryptojobslist.com/', @unparse, @parsed_page).scrape

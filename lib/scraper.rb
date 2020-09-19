9212af9d09bb6a3a6b6dff237efcf348603f387c
Muhammed Cihad Kurel <mcihadkurel@gmail.com>
2020-09-19 16:26:20 +0300

    Fix rubocop errors

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -31,11 +31,10 @@ class Scraper
     end
     jobs.count
   end
-  
+
   private
 
-def hostel_card
-  parsed_page.css('li.job.inline.featured')
-end
-  
+  def hostel_card
+    parsed_page.css('li.job.inline.featured')
+  end
 end

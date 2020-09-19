d40cdf4c3ffc0bcf1e2eb8bb7993bde4823ce3da
Muhammed Cihad Kurel <65771498+mcihadkurel@users.noreply.github.com>
2020-09-19 16:24:32 +0300

    Remove private class

--- bin/main.rb
+++ bin/main.rb
@@ -3,10 +3,4 @@
 require 'csv'
 require_relative '../lib/scraper.rb'
 
-private
-
-def hostel_card
-  parsed_page.css('li.job.inline.featured')
-end
-
 Scraper.new('https://cryptojobslist.com/', @unparse, @parsed_page).scrape
